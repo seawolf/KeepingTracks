e27cd72b7a7d84d947ea923b49b75e8c6c1e9466
Ben Arnold <ben@seawolfsanctuary.com>
2012-04-12 19:33:12 +0100

    Add: read text file lines into array

--- src/com/seawolfsanctuary/tmt/FromActivity.java
+++ src/com/seawolfsanctuary/tmt/FromActivity.java
@@ -1,11 +1,35 @@
 package com.seawolfsanctuary.tmt;
 
+import java.io.InputStream;
+
 import android.app.Activity;
 import android.os.Bundle;
 
 public class FromActivity extends Activity {
+
+	private String[] read_csv(String filename) {
+		String[] array = {};
+
+		try {
+			InputStream input;
+			input = getAssets().open("stations.lst");
+			int size = input.available();
+			byte[] buffer = new byte[size];
+
+			input.read(buffer);
+			input.close();
+			array = new String(buffer).split("\n");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return array;
+	}
+
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.tab_from);
 	}
+
 }
