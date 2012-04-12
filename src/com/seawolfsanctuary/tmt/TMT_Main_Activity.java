5cb373b2582586cc9be45f9bda302a42bda1cdd0
Ben Arnold <ben@seawolfsanctuary.com>
2012-04-12 19:17:33 +0100

    Add: three tabs (overrides main layout)

--- src/com/seawolfsanctuary/tmt/TMT_Main_Activity.java
+++ src/com/seawolfsanctuary/tmt/TMT_Main_Activity.java
@@ -12,5 +12,29 @@ public class TMT_Main_Activity extends TabActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Resources res = getResources(); // Resource object to get Drawables
+        TabHost tabHost = getTabHost();  // The activity TabHost
+        TabHost.TabSpec spec;  // Reusable TabSpec for each tab
+        Intent intent;  // Reusable Intent for each tab
+
+        // Create an Intent to launch an Activity for the tab (to be reused)
+        intent = new Intent().setClass(this, FromActivity.class);
+
+        // Initialise a TabSpec for each tab and add it to the TabHost
+        spec = tabHost.newTabSpec("from").setIndicator("From").setContent(intent);
+        tabHost.addTab(spec);
+
+        // Do the same for the other tabs
+        intent = new Intent().setClass(this, DetailActivity.class);
+        spec = tabHost.newTabSpec("detail").setIndicator("Detail").setContent(intent);
+        tabHost.addTab(spec);
+
+        intent = new Intent().setClass(this, ToActivity.class);
+        spec = tabHost.newTabSpec("to").setIndicator("To").setContent(intent);
+        tabHost.addTab(spec);
+
+        tabHost.setCurrentTab(0);
+
     }
 }
