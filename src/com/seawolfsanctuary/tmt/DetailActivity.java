8ae352017a529fc2ab760f8a57d62280556a5691
Ben Arnold <ben@seawolfsanctuary.com>
2012-04-12 19:25:24 +0100

    Add: layouts of each tab

---
+++ src/com/seawolfsanctuary/tmt/DetailActivity.java
@@ -0,0 +1,11 @@
+package com.seawolfsanctuary.tmt;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DetailActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab_detail);
+    }
+}
---
+++ src/com/seawolfsanctuary/tmt/ToActivity.java
@@ -0,0 +1,11 @@
+package com.seawolfsanctuary.tmt;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ToActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab_to);
+    }
+}
