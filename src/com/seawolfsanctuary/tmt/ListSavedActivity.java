847f939cf21bc34a662e8ff9fe823751874f6141
Ben Arnold <ben@seawolfsanctuary.com>
2012-04-13 23:14:27 +0100

    Add: basic CSV file reader into set arrays/hashtables

--- src/com/seawolfsanctuary/tmt/ListSavedActivity.java
+++ src/com/seawolfsanctuary/tmt/ListSavedActivity.java
@@ -1,8 +1,16 @@
 package com.seawolfsanctuary.tmt;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Iterator;
+
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -42,8 +50,30 @@ public class ListSavedActivity extends ListActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+		ArrayList<String> entries = new ArrayList<String>();
+		ArrayList<String> names = new ArrayList<String>();
+		final Hashtable<String, String> data = new Hashtable<String, String>();
+
+		try {
+			entries = loadSavedEntries();
+
+			for (Iterator<String> i = entries.iterator(); i.hasNext();) {
+				String str = (String) i.next();
+
+				String name = Helpers.trimCSVSpeech(str.split(",")[0]);
+				names.add(name);
+				data.put(name + "_a", Helpers.trimCSVSpeech(str.split(",")[1]));
+				data.put(name + "_b", Helpers.trimCSVSpeech(str.split(",")[2]));
+
+			}
+
+		} catch (Exception e) {
+			Toast.makeText(getApplicationContext(), "Error: " + e.getMessage(),
+					Toast.LENGTH_LONG).show();
+		}
+
 		setListAdapter(new ArrayAdapter<String>(this,
-				R.layout.list_saved_activity, loadSavedEntries()));
+				R.layout.list_saved_activity, names));
 
 		ListView lv = getListView();
 		lv.setTextFilterEnabled(true);
@@ -51,18 +81,61 @@ public class ListSavedActivity extends ListActivity {
 		lv.setOnItemClickListener(new OnItemClickListener() {
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
-				Toast.makeText(getApplicationContext(),
-						((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+				Toast.makeText(
+						getApplicationContext(),
+						data.get(((TextView) view).getText() + "_a") + " | "
+								+ data.get(((TextView) view).getText() + "_b"),
+						Toast.LENGTH_SHORT).show();
 			}
 		});
 
 	}
 
-	private String[] loadSavedEntries() {
-		return new String[] { loadSavedEntry("1"), loadSavedEntry("2") };
-	}
+	private ArrayList<String> loadSavedEntries() {
+		boolean mExternalStorageReadable = false;
+		String state = Environment.getExternalStorageState();
 
-	private String loadSavedEntry(String name) {
-		return name;
+		if (Environment.MEDIA_MOUNTED.equals(state)) {
+			// We can read and write the media
+			mExternalStorageReadable = true;
+		} else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+			// We can only read the media
+			mExternalStorageReadable = true;
+		} else {
+			// Something else is wrong. It may be one of many other states, but
+			// all we need to know is we can neither read nor write
+			mExternalStorageReadable = false;
+		}
+
+		if (mExternalStorageReadable) {
+			try {
+				String line = null;
+				ArrayList<String> array = new ArrayList<String>();
+
+				File f = new File(Environment.getExternalStorageDirectory()
+						.toString(), "/tmt.csv");
+				BufferedReader reader = new BufferedReader(new FileReader(f));
+
+				while ((line = reader.readLine()) != null) {
+					String str = new String(line);
+					array.add(str);
+				}
+
+				reader.close();
+				Toast.makeText(getBaseContext(), "CSV file loaded.",
+						Toast.LENGTH_SHORT).show();
+
+				return array;
+
+			} catch (Exception e) {
+				Toast.makeText(getBaseContext(), "Error: " + e.getMessage(),
+						Toast.LENGTH_LONG).show();
+
+				return new ArrayList<String>();
+			}
+
+		} else {
+			return new ArrayList<String>();
+		}
 	}
 }
