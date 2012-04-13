c3ff9fe2495cca1122a899728a9de33d8e577a58
Ben Arnold <ben.arnold@yodel.co.uk>
2012-04-13 10:58:38 +0100

    Add: class checkbox and text field on details tab

--- src/com/seawolfsanctuary/tmt/DetailActivity.java
+++ src/com/seawolfsanctuary/tmt/DetailActivity.java
@@ -2,10 +2,21 @@ package com.seawolfsanctuary.tmt;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class DetailActivity extends Activity {
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.tab_detail);
-    }
+	CheckBox cb_DetailClass;
+	TextView txt_DetailClass;
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tab_detail);
+	}
+
+	public void onCheckboxClicked(View v) {
+		TextView txt_DetailClass = (TextView) findViewById(R.id.txt_DetailClass);
+		txt_DetailClass.setEnabled(((CheckBox) v).isChecked());
+	}
 }
