fad388b2791da71374fe12f9c70318c11943b3e2
Ben Arnold <ben@seawolfsanctuary.com>
2012-04-16 11:47:02 +0100

    Add: meaningful variable names

--- src/com/seawolfsanctuary/tmt/DeepDiveListSavedActivity.java
+++ src/com/seawolfsanctuary/tmt/DeepDiveListSavedActivity.java
@@ -1,6 +1,7 @@
 package com.seawolfsanctuary.tmt;
 
 import android.app.ExpandableListActivity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,74 +15,70 @@ public class DeepDiveListSavedActivity extends ExpandableListActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		setListAdapter(new DeepDiveListSavedAdapter());
+		setListAdapter(new DeepDiveListSavedAdapter(getBaseContext()));
 		ExpandableListView lv = getExpandableListView();
 		lv.setTextFilterEnabled(true);
 	}
 
 	class DeepDiveListSavedAdapter extends BaseExpandableListAdapter {
+		private Context context;
+
+		public DeepDiveListSavedAdapter(Context context) {
+			context = context;
+		}
 
 		@Override
-		public Object getChild(int arg0, int arg1) {
-			// TODO Auto-generated method stub
+		public Object getChild(int groupPosition, int childPosition) {
 			return null;
 		}
 
 		@Override
-		public long getChildId(int arg0, int arg1) {
-			// TODO Auto-generated method stub
+		public long getChildId(int groupPosition, int childPosition) {
 			return 0;
 		}
 
 		@Override
-		public View getChildView(int arg0, int arg1, boolean arg2, View arg3,
-				ViewGroup arg4) {
-			// TODO Auto-generated method stub
+		public View getChildView(int groupPosition, int childPosition,
+				boolean isLastChild, View convertView, ViewGroup parent) {
 			return null;
 		}
 
 		@Override
-		public int getChildrenCount(int arg0) {
-			// TODO Auto-generated method stub
+		public int getChildrenCount(int groupPosition) {
 			return 0;
 		}
 
 		@Override
-		public Object getGroup(int arg0) {
-			// TODO Auto-generated method stub
+		public Object getGroup(int groupPosition) {
 			return null;
 		}
 
 		@Override
 		public int getGroupCount() {
-			// TODO Auto-generated method stub
 			return 0;
 		}
 
 		@Override
-		public long getGroupId(int arg0) {
-			// TODO Auto-generated method stub
+		public long getGroupId(int groupPosition) {
 			return 0;
 		}
 
 		@Override
-		public View getGroupView(int arg0, boolean arg1, View arg2,
-				ViewGroup arg3) {
-			// TODO Auto-generated method stub
+		public View getGroupView(int groupPosition, boolean isExpanded,
+				View convertView, ViewGroup parent) {
 			return null;
 		}
 
 		@Override
 		public boolean hasStableIds() {
-			// TODO Auto-generated method stub
 			return false;
 		}
 
 		@Override
-		public boolean isChildSelectable(int arg0, int arg1) {
-			// TODO Auto-generated method stub
+		public boolean isChildSelectable(int groupPosition, int childPosition) {
 			return false;
 		}
 
 	}
+
 }
