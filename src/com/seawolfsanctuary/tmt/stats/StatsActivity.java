506a0aaf082ff0ba9f827b3e8b02c19e42ea86ff
Ben Arnold <ben@seawolfsanctuary.com>
2012-10-03 08:25:10 +0100

    Add: first stats activity

---
+++ src/com/seawolfsanctuary/tmt/stats/StatsActivity.java
@@ -0,0 +1,10 @@
+package com.seawolfsanctuary.tmt.stats;
+
+public class StatsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.stats_activity);
+	}
+}
