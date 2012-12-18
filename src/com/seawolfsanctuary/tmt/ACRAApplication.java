ef04452768ac3398bd0ab926c00ff77cb2c8afe0
Ben Arnold <ben@seawolfsanctuary.com>
2012-12-18 20:32:08 +0000

    Del: unnecessary calls and definitions

--- src/com/seawolfsanctuary/tmt/ACRAApplication.java
+++ src/com/seawolfsanctuary/tmt/ACRAApplication.java
@@ -5,16 +5,12 @@ import org.acra.ReportingInteractionMode;
 import org.acra.annotation.ReportsCrashes;
 
 import android.app.Application;
-import android.content.Intent;
 
-@ReportsCrashes(formKey = "", mailTo = "bugs@seawolfsanctuary.com", mode = ReportingInteractionMode.NOTIFICATION, resToastText = R.string.crash_toast_text, resNotifTickerText = R.string.crash_notif_ticker_text, resNotifTitle = R.string.crash_notif_title, resNotifText = R.string.crash_notif_text, resNotifIcon = android.R.drawable.stat_notify_error, resDialogText = R.string.crash_dialog_text, resDialogIcon = android.R.drawable.ic_dialog_info, resDialogTitle = R.string.crash_dialog_title, resDialogCommentPrompt = R.string.crash_dialog_comment_prompt, resDialogOkToast = R.string.crash_dialog_ok_toast)
+@ReportsCrashes(formKey = "", mailTo = "bugs@seawolfsanctuary.com", mode = ReportingInteractionMode.NOTIFICATION, resNotifTickerText = R.string.crash_notif_title, resNotifTitle = R.string.crash_notif_title, resNotifText = R.string.crash_notif_text, resNotifIcon = android.R.drawable.stat_notify_error, resDialogText = R.string.crash_dialog_text, resDialogIcon = android.R.drawable.ic_dialog_info, resDialogTitle = R.string.crash_dialog_title, resDialogCommentPrompt = R.string.crash_dialog_comment_prompt, resDialogOkToast = R.string.crash_dialog_ok_toast)
 public class ACRAApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
 		ACRA.init(this);
-		Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
-		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		startActivity(intent);
 	}
 }
