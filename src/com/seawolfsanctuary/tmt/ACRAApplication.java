db0f25533652c81f2a23b6351143d9816086abe7
Ben Arnold <ben@seawolfsanctuary.com>
2012-12-18 19:26:34 +0000

    Add: starting MenuActivity while protected by ACRA

--- src/com/seawolfsanctuary/tmt/ACRAApplication.java
+++ src/com/seawolfsanctuary/tmt/ACRAApplication.java
@@ -5,6 +5,7 @@ import org.acra.ReportingInteractionMode;
 import org.acra.annotation.ReportsCrashes;
 
 import android.app.Application;
+import android.content.Intent;
 
 @ReportsCrashes(formKey = "", mailTo = "bugs@seawolfsanctuary.com", mode = ReportingInteractionMode.TOAST, resToastText = R.string.crash_toast_text, resNotifTickerText = R.string.crash_notif_ticker_text, resNotifTitle = R.string.crash_notif_title, resNotifText = R.string.crash_notif_text, resNotifIcon = android.R.drawable.stat_notify_error, resDialogText = R.string.crash_dialog_text, resDialogIcon = android.R.drawable.ic_dialog_info, resDialogTitle = R.string.crash_dialog_title, resDialogCommentPrompt = R.string.crash_dialog_comment_prompt, resDialogOkToast = R.string.crash_dialog_ok_toast)
 public class ACRAApplication extends Application {
@@ -12,5 +13,9 @@ public class ACRAApplication extends Application {
 	public void onCreate() {
 		super.onCreate();
 		ACRA.init(this);
+		System.out.println("ACRA initialised. Crashes will be reported.");
+		Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		startActivity(intent);
 	}
 }
