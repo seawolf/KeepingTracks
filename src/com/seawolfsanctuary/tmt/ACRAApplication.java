bcb900e2a9105ce561cc1fd200486362eaa84236
Ben Arnold <ben@seawolfsanctuary.com>
2012-12-18 19:25:41 +0000

    Add: basic ACRA set-up with dialog notification crash catcher

---
+++ src/com/seawolfsanctuary/tmt/ACRAApplication.java
@@ -0,0 +1,16 @@
+package com.seawolfsanctuary.tmt;
+
+import org.acra.ACRA;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+
+import android.app.Application;
+
+@ReportsCrashes(formKey = "", mailTo = "bugs@seawolfsanctuary.com", mode = ReportingInteractionMode.TOAST, resToastText = R.string.crash_toast_text, resNotifTickerText = R.string.crash_notif_ticker_text, resNotifTitle = R.string.crash_notif_title, resNotifText = R.string.crash_notif_text, resNotifIcon = android.R.drawable.stat_notify_error, resDialogText = R.string.crash_dialog_text, resDialogIcon = android.R.drawable.ic_dialog_info, resDialogTitle = R.string.crash_dialog_title, resDialogCommentPrompt = R.string.crash_dialog_comment_prompt, resDialogOkToast = R.string.crash_dialog_ok_toast)
+public class ACRAApplication extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		ACRA.init(this);
+	}
+}
