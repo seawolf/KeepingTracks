c1547a8be36c4e1a76f0bade83cb1100e8946f91
Ben Arnold <ben@seawolfsanctuary.com>
2013-01-13 17:42:31 +0000

    Add: shared preferences access

--- src/com/seawolfsanctuary/tmt/UserPrefsActivity.java
+++ src/com/seawolfsanctuary/tmt/UserPrefsActivity.java
@@ -1,14 +1,29 @@
 package com.seawolfsanctuary.tmt;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class UserPrefsActivity extends Activity {
+	public static final String PREFS_APP = "ApplicationPrefs";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.user_prefs_activity);
+
+		SharedPreferences settings = getSharedPreferences(PREFS_APP, 0);
 	}
 
+	@Override
+	protected void onStop() {
+		super.onStop();
+
+		SharedPreferences settings = getSharedPreferences(PREFS_APP, 0);
+		SharedPreferences.Editor editor = settings.edit();
+		// boolean preferenceValue = ... ;
+		// editor.putBoolean("preferenceName", preferenceValue);
+
+		editor.commit();
+	}
 }
