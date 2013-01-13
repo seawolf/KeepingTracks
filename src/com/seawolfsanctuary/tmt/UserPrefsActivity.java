a0b99052cc6ba2a29c71bac104517f1c7676a319
Ben Arnold <ben@seawolfsanctuary.com>
2013-01-13 17:45:32 +0000

    Add: remove previous crash reports so message doesn't appear on start

--- src/com/seawolfsanctuary/tmt/UserPrefsActivity.java
+++ src/com/seawolfsanctuary/tmt/UserPrefsActivity.java
@@ -1,8 +1,12 @@
 package com.seawolfsanctuary.tmt;
 
+import java.io.File;
+
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class UserPrefsActivity extends Activity {
 	public static final String PREFS_APP = "ApplicationPrefs";
@@ -25,5 +29,33 @@ public class UserPrefsActivity extends Activity {
 		// editor.putBoolean("preferenceName", preferenceValue);
 
 		editor.commit();
+	}
+
+	public void clearCrashReports(View v) {
+		File appFiles = getFilesDir(); // actually directory
+		File[] listOfFiles = appFiles.listFiles();
+		System.out.println("Found " + listOfFiles.length + " file(s) in "
+				+ appFiles.getAbsolutePath());
+
+		if (listOfFiles.length == 0) {
+			Toast.makeText(getBaseContext(), "No previous reports found.",
+					Toast.LENGTH_SHORT).show();
+		} else {
+			String fileInList;
+			int filesRemoved = 0;
+			for (File file : listOfFiles) {
+				if (file.isFile()) {
+					fileInList = file.getName();
+					if (fileInList.endsWith(".stacktrace")) {
+						System.out.println("Removing: " + fileInList);
+						file.delete();
+						filesRemoved += 1;
+					}
+				}
+			}
+			Toast.makeText(getBaseContext(),
+					"" + filesRemoved + " previous reports removed.",
+					Toast.LENGTH_SHORT).show();
+		}
 	}
 }
